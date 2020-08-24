bef772f8e7947aa0ba147c5dfe0e421e21710dec
mnmsnrk <69619645+mnmsnrk@users.noreply.github.com>
2020-08-24 15:17:01 +0900

    Revert "Revert "AjaxAppの準備""
    This reverts commit 5e8091448fb9a252c558c396312a6e9ee0966f4b.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'posts',to:'posts#index'
 end
 Rails.application.routes.draw do
-  get 'post',to:'posts#index'
-  get 'posts/new',to:'posts#new'
+  root to: 'posts#index'
   post 'posts',to:'posts#create'
 end
